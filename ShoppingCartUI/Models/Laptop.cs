95c86b7801e1a7ab25da6bc32d214c1549db11fe
Alex <asdfjoeasdfjoe@gmail.com>
2023-12-10 01:22:20 +0800

    Add Laptop class

---
+++ ShoppingCartUI/Models/Laptop.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCartUI.Models
+{
+    [Table("Laptop")]
+    public class Laptop
+    {
+        public int Id { get; set; }
+        public string ModelName { get; set; } = string.Empty;
+        [Required]
+        public double Price { get; set; }
+        public string? Image { get; set; }
+        [Required]
+        public int BrandId { get; set; }
+        #pragma warning disable CS8618
+        public Brand Brand;
+
+    }
+}
