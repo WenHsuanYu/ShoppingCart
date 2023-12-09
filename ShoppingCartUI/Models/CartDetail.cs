d65cc0fbbc9c132eae0eedf3cd1293ec0e614e1d
Alex <asdfjoeasdfjoe@gmail.com>
2023-12-10 02:19:28 +0800

    Add `CartDetail` class

---
+++ ShoppingCartUI/Models/CartDetail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCartUI.Models
+{
+    [Table("CartDetail")]
+    public class CartDetail
+    {
+        public int Id { get; set; }
+        [Required]
+        public int ShoppingCartId { get; set; }
+        [Required]
+        public int LaptopId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public Laptop Laptop { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+    }
+}
