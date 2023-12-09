d0ddbaea24ffd667ad76ef554c0866d62c9a2599
Alex <asdfjoeasdfjoe@gmail.com>
2023-12-10 01:22:46 +0800

    Add Brand class

---
+++ ShoppingCartUI/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCartUI.Models
+{
+    [Table("Brands")]
+    public class Brand
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40, ErrorMessage = "Brand name cannot be longer than 40 characters.")]
+        public string BrandName { get; set; }
+        public List<Laptop> Laptops { get; set; }
+    }
+}
