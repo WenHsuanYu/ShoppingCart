830c5cde5c493e154ebe79199cd7641f68f6f037
Alex <asdfjoeasdfjoe@gmail.com>
2023-12-10 02:21:14 +0800

    Add `Order` class

---
+++ ShoppingCartUI/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCartUI.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+
+        public bool IsDeleted { get; set; } = false;
+        public int OrderStatusId { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderDetail> OrderDetail { get; set; }
+    }
+}
