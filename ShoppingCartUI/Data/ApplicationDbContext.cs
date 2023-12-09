92a3356a692cf37d925ea642c829820baf38de71
Alex <asdfjoeasdfjoe@gmail.com>
2023-12-10 02:26:33 +0800

    Add DbSet properties for database tables/entities
    Add DbSet properties for database tables/entities: Brand, Laptop, ShoppingCart, CartDetail, Order, OrderDetail, and OrderStatus.

--- ShoppingCartUI/Data/ApplicationDbContext.cs
+++ ShoppingCartUI/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ShoppingCartUI.Models;
 
 namespace ShoppingCartUI.Data
 {
@@ -9,5 +10,14 @@ namespace ShoppingCartUI.Data
             : base(options)
         {
         }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Laptop> Laptops { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+
     }
 }
