78c34efbceb38de74f2a70afbd0c5b5e3cdfda6c
Alex <asdfjoeasdfjoe@gmail.com>
2023-12-10 02:21:37 +0800

    Add  `OrderDetail`

---
+++ ShoppingCartUI/Models/OrderDetail.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCartUI.Models
+{
+    [Table("OrderDetail")]
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+        [Required]
+        public int LaptopId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public double UnitPrice { get; set; }
+        public Order Order { get; set; }
+        public Laptop Laptop { get; set; }
+         
+    }
+}
