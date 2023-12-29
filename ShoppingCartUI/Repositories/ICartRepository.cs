78facc4e2609b2e5c4e94276dfb78177c222b537
Alex <asdfjoeasdfjoe@gmail.com>
2023-12-29 15:51:02 +0800

    Add interface of service `CartRepository`

---
+++ ShoppingCartUI/Repositories/ICartRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingCartUI.Repositories
+{
+    public interface ICartRepository
+    {
+        Task<bool> AddItem(int LaptopId, int quantity);
+        Task<bool> RemoveItem(int LaptopId);
+        Task<ShoppingCart?> GetUserCart();
+        Task<ShoppingCart?> GetShoppingCart(string userId);
+    }
+}
