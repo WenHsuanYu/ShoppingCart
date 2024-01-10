932f54a47b4fb8e608fda0e1a0cdd766612cb01e
Alex <asdfjoeasdfjoe@gmail.com>
2024-01-10 16:00:31 +0800

    Create UserOrderController handling requests

---
+++ ShoppingCartUI/Controllers/UserOrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingCartUI.Controllers
+{
+    [Authorize]
+    public class UserOrderController : Controller
+    {
+        private readonly IUserOrderRepository _userOrderRepository;
+
+        public UserOrderController(IUserOrderRepository userOrderRepository)
+        {
+            _userOrderRepository = userOrderRepository;
+        }
+
+        public async Task<IActionResult> UserOrders()
+        {
+            var orders = await _userOrderRepository.GetUserOrders();
+            return View(orders);
+        }
+    }
+}
