1c90de2a9b94a833c1d14ec7238dc202c7bc9073
Alex <asdfjoeasdfjoe@gmail.com>
2024-02-21 15:55:16 +0800

    Change property name of Laptop

---
+++ ShoppingCartUI/Data/Migrations/20240219025519_Change Name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingCartUI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Laptop",
+                newName: "ImageFileName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageFileName",
+                table: "Laptop",
+                newName: "Image");
+        }
+    }
+}
