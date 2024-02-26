8a4f5437948b1cddde63b8aaad6cc45336f453a3
Alex <asdfjoeasdfjoe@gmail.com>
2024-02-26 19:57:25 +0800

    Add `ImageUrl` model

---
+++ ShoppingCartUI/Models/ImageUrl.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ShoppingCartUI.Attributes;
+
+namespace ShoppingCartUI.Models
+{
+    [Table("ImageUrl")]
+    public class ImageUrl
+    {
+        [Key]
+        public int Id {  get; set; }
+
+        public required string DeleteHash { get; set; }
+        [DataType(DataType.Url)]
+        public required string? Url { get; set; }
+
+        public int LaptopId { get; set; }
+
+        public Laptop? Laptop { get; set; }
+    }
+}
