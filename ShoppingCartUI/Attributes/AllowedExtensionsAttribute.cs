18d650870ee7f1a9c78defe52aea713849ae7bf7
Alex <asdfjoeasdfjoe@gmail.com>
2024-02-27 16:45:03 +0800

    Check file signature

--- ShoppingCartUI/Attributes/AllowedExtensionsAttribute.cs
+++ ShoppingCartUI/Attributes/AllowedExtensionsAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using System.Diagnostics.CodeAnalysis;
+using static System.Net.Mime.MediaTypeNames;
 namespace ShoppingCartUI.Attributes
 {
     public class AllowedExtensionsAttribute : ValidationAttribute
@@ -15,11 +16,11 @@ namespace ShoppingCartUI.Attributes
         {
             return $"This photo extension is not allowed!";
         }
-
-        protected override ValidationResult IsValid(
+        protected override ValidationResult? IsValid(
                 object? value, ValidationContext validationContext)
         {
-            var file = value as IFormFile;
+            IFormFile? file = value as IFormFile;
+            bool flag = false;
             if (file != null)
             {
                 var extension = Path.GetExtension(file.FileName);
@@ -27,9 +28,54 @@ namespace ShoppingCartUI.Attributes
                 {
                     return new ValidationResult(GetErrorMessage());
                 }
+                //Check file signature (Byte Order Mark)
+                using var memoryStream = new MemoryStream();
+                file.CopyTo(memoryStream);
+                byte[] fileData = memoryStream.ToArray();
+                List<byte[]> sig = fileSignature[extension.ToUpper()];
+                
+                foreach (byte[] b in sig)
+                {
+                    var curFileSig = new byte[b.Length];
+                    Array.Copy(fileData, curFileSig, b.Length);
+                    if (curFileSig.SequenceEqual(b))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
             }
-
-            return ValidationResult.Success;
+            return flag == true ? ValidationResult.Success : new ValidationResult(GetErrorMessage());
         }
+
+        protected static Dictionary<string, List<byte[]>> fileSignature = new Dictionary<string, List<byte[]>>
+        {
+            { ".PNG", new List<byte[]> 
+                                    { 
+                                        new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A } 
+                                    }
+            },
+            { ".JPG", new List<byte[]>
+                                    {
+                                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 },
+                                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE1 },
+                                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE8 }
+                                    }
+            },
+            { ".JPEG", new List<byte[]>
+                                    {
+                                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 },
+                                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE2 },
+                                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE3 }
+                                    }
+            },
+            { ".WEBP", new List<byte[]>
+
+                                    {
+                                        new byte[] { 0x52, 0x49, 0x46, 0x46 },
+                                        new byte[] { 0x57, 0x45, 0x42, 0x50 }
+                                    }
+            }
+        };
     }
 }
